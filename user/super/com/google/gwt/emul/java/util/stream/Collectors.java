365ff9482ef048572ab10970a82834d169b5a327
Colin Alworth <niloc132@gmail.com>
2015-10-30 12:35:01 -0500

    Collector impls
    Change-Id: Ic8854cd2da9f82f287734631119ae434536754ca

--- user/super/com/google/gwt/emul/java/util/stream/Collectors.java
+++ user/super/com/google/gwt/emul/java/util/stream/Collectors.java
@@ -1,5 +1,7 @@
 package java.util.stream;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.concurrent.ConcurrentMap;
 import java.util.Collection;
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.function.ToDoubleFunction;
+import java.util.stream.Collector;
 
 
 public final class Collectors {
@@ -19,38 +22,39 @@ public final class Collectors {
   //...TODO
   
   public static <T,C extends Collection<T>> Collector<T,?,C> toCollection(Supplier<C> collectionFactory) {
-    return null;//TODO
+    return Collector.of(collectionFactory, Collection::add, Collection::addAll);
   }
 
+  //not supported
 //  public static <T,K,U> Collector<T,?,ConcurrentMap<K,U>> toConcurrentMap(Function<? super T,? extends K> keyMapper, Function<? super T,? extends U> valueMapper) {
 //    return null;
 //  }
-
+//
 //  public static <T,K,U> Collector<T,?,ConcurrentMap<K,U>> toConcurrentMap(Function<? super T,? extends K> keyMapper, Function<? super T,? extends U> valueMapper, BinaryOperator<U> mergeFunction) {
 //    return null;
 //  }
-
-  public static <T,K,U,M extends ConcurrentMap<K,U>> Collector<T,?,M> toConcurrentMap(Function<? super T,? extends K> keyMapper, Function<? super T,? extends U> valueMapper, BinaryOperator<U> mergeFunction, Supplier<M> mapSupplier) {
-    return null;//TODO
-  }
+//
+//  public static <T,K,U,M extends ConcurrentMap<K,U>> Collector<T,?,M> toConcurrentMap(Function<? super T,? extends K> keyMapper, Function<? super T,? extends U> valueMapper, BinaryOperator<U> mergeFunction, Supplier<M> mapSupplier) {
+//    return null;
+//  }
 
   public static <T> Collector<T,?,List<T>> toList() {
-    return null;//TODO
+    return Collector.of(ArrayList::new, List::add, List::addAll);
   }
 
-  public static <T,K,U> Collector<T,?,Map<K,U>> toMap(Function<? super T,? extends K> keyMapper, Function<? super T,? extends U> valueMapper) {
-    return null;//TODO
+  public static <T,K,U> Collector<T,?,Map<K,U>> toMap(final Function<? super T,? extends K> keyMapper, final Function<? super T,? extends U> valueMapper) {
+    return Collector.of(HashMap::new, (map, item) -> map.put(keyMapper.apply(item), valueMapper.apply(item)), HashMap::putAll);
   }
 
   public static <T,K,U> Collector<T,?,Map<K,U>> toMap(Function<? super T,? extends K> keyMapper, Function<? super T,? extends U> valueMapper, BinaryOperator<U> mergeFunction) {
-    return null;//TODO
+    return toMap(keyMapper, valueMapper);
   }
 
-  public static <T,K,U,M extends Map<K,U>> Collector<T,?,M> toMap(Function<? super T,? extends K> keyMapper, Function<? super T,? extends U> valueMapper, BinaryOperator<U> mergeFunction, Supplier<M> mapSupplier) {
-    return null;//TODO
+  public static <T,K,U,M extends Map<K,U>> Collector<T,?,M> toMap(final Function<? super T,? extends K> keyMapper, final Function<? super T,? extends U> valueMapper, BinaryOperator<U> mergeFunction, Supplier<M> mapSupplier) {
+    return Collector.of(mapSupplier, (map, item) -> map.put(keyMapper.apply(item), valueMapper.apply(item)), HashMap::putAll);
   }
 
   public static <T> Collector<T,?,Set<T>> toSet() {
-    return null;//TODO
+    return Collector.of(HashSet::new, Set::add, Set::addAll);
   }
 }
